0b5a4c8bf1735b0569cbadc5b2ae9dc6629758d9
Michal Strehovský <michals@microsoft.com>
2019-02-03 19:06:46 +0100

    Debug info

--- with-runtime/build.cmd
+++ with-runtime/build.cmd
@@ -9,6 +9,6 @@
 
 @if "%1" == "clean" exit /B
 
-csc /debug:embedded /noconfig /nostdlib /runtimemetadataversion:v4.0.30319 zerosharp.cs /out:zerosharp.ilexe /langversion:latest /unsafe /r:Test.CoreLib.dll
-"%ILCPATH%\ilc" zerosharp.ilexe -r:Test.CoreLib.dll -o zerosharp.obj --systemmodule Test.CoreLib --map zerosharp.map -O --noscan
-link zerosharp.obj "%SDKPATH%\bootstrapper.lib" "%SDKPATH%\Runtime.lib" kernel32.lib ole32.lib advapi32.lib /incremental:no
+csc /debug /noconfig /nostdlib /runtimemetadataversion:v4.0.30319 zerosharp.cs /out:zerosharp.ilexe /langversion:latest /unsafe /r:Test.CoreLib.dll
+"%ILCPATH%\ilc" zerosharp.ilexe -r:Test.CoreLib.dll -o zerosharp.obj --systemmodule Test.CoreLib --map zerosharp.map -O --noscan -g
+link zerosharp.obj "%SDKPATH%\bootstrapper.lib" "%SDKPATH%\Runtime.lib" kernel32.lib ole32.lib advapi32.lib /incremental:no /debug /opt:ref
